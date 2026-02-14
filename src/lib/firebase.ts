0dd12dc46aacae2e43321b86e7e132365726ecdf
Stephan-Heuscher <x+github.com@yic.ch>
2026-02-14 18:33:17 +0100

    fix: move firebase config to env vars to resolve secret alert

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCW34kPaNI7ruFlEfy9j4X6DPM0mgRunKw",
-    authDomain: "insel-glossar.firebaseapp.com",
-    projectId: "insel-glossar",
-    storageBucket: "insel-glossar.firebasestorage.app",
-    messagingSenderId: "1049814310953",
-    appId: "1:1049814310953:web:578b810852e38396b27c38",
-    measurementId: "G-3NXPJR6C0H"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
